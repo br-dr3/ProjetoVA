4042c5d37b78ad3047c2c9caa1c32ef65928ff1e
Andreossi <brian.andreossi@accenture.com>
2019-04-13 21:48:29 -0300

    First Commit

---
+++ src/core/Main.java
@@ -0,0 +1,13 @@
+package core;
+
+import model.Rule;
+
+public class Main {
+	public static void main (String args[]) {
+		Rule rule1 = new Rule(110);
+		Rule rule2 = new Rule(66);
+		Rule rule3 = new Rule(80);
+		
+		System.out.println(rule1);	
+	}
+}
